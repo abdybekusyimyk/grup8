4c578bf62667a7381f71248a24e2e2530e552100
abdubekusyimyk <abdybekuulusyimyk@gmail.com>
2023-05-20 16:14:57 +0600

    animation

---
+++ genric/android/app/src/main/kotlin/com/example/genric/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.genric
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ moon_light_an/android/app/src/main/kotlin/com/example/moon_light_an/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moon_light_an
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
