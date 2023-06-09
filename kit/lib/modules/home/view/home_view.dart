cfe46e783eb8f357ccdd697c9b82dd3aebdeb635
abdubekusyimyk <abdybekuulusyimyk@gmail.com>
2023-06-10 02:50:50 +0600

    visa

--- kit/lib/modules/home/view/home_view.dart
+++ kit/lib/modules/home/view/home_view.dart
@@ -36,125 +36,189 @@ class HomeView extends StatelessWidget {
         children: [
           Expanded(
             // flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Column(
-                      // mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Spacer(),
-                        const Text('Todal baiance'),
-                        const SizedBox(height: 7),
-                        const Text.rich(
-                          TextSpan(
-                            children: [
-                              TextSpan(
-                                text: '£ 23,970.',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              TextSpan(text: '30'),
-                            ],
-                            style: TextStyle(fontSize: 30),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Spacer(),
+                      const Text('Todal baiance'),
+                      const SizedBox(height: 7),
+                      const Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: '£ 23,970.',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(text: '30'),
+                          ],
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      const Spacer(),
+                      const Text('This month'),
+                      const SizedBox(height: 7),
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            'assets/icons/Vector 1.svg',
+                            // fit: BoxFit.scaleDown,
+                            width: 20,
                           ),
-                        ),
-                        const Spacer(),
-                        const Text('This month'),
-                        const SizedBox(height: 7),
-                        Row(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/icons/Vector 1.svg',
-                              // fit: BoxFit.scaleDown,
-                              width: 20,
+                          const SizedBox(width: 15),
+                          const Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: '£ 5,235.',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(text: '25'),
+                              ],
+                              style: TextStyle(fontSize: 30),
                             ),
-                            const SizedBox(width: 15),
-                            const Text.rich(
-                              TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text: '£ 5,235.',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  TextSpan(text: '25'),
-                                ],
-                                style: TextStyle(fontSize: 30),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/icons/down.svg',
-                              // fit: BoxFit.scaleDown,
-                              width: 20,
-                            ),
-                            SizedBox(width: 15),
-                            Text.rich(
-                              TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text: '£ 3,710.',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  TextSpan(text: '80'),
-                                ],
-                                style: TextStyle(fontSize: 30),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Spacer(),
-                      ],
-                    ),
-                    const SizedBox(width: 35),
-                    Container(
-                      width: 350,
-                      height: 280,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25),
-                        gradient: const LinearGradient(
-                            colors: [
-                              Colors.teal,
-                              // Colors.tealAccent,
-                              // Colors.black87,
-                              Colors.redAccent,
-                              Colors.yellow
-                            ],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.centerRight),
+                          ),
+                        ],
                       ),
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            'assets/icons/down.svg',
+                            // fit: BoxFit.scaleDown,
+                            width: 20,
+                          ),
+                          const SizedBox(width: 15),
+                          const Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: '£ 3,710.',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(text: '80'),
+                              ],
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Spacer(),
+                    ],
+                  ),
+                  const SizedBox(width: 35),
+                  Container(
+                    width: 365,
+                    height: 280,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(22),
+                      gradient: const LinearGradient(
+                          colors: [
+                            Colors.teal,
+                            // Colors.tealAccent,
+                            // Colors.black87,
+                            Colors.redAccent,
+                            Colors.yellow
+                          ],
+                          begin: Alignment.bottomLeft,
+                          end: Alignment.centerRight),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0),
                       child: Column(
-                        children: [],
-                      ),
-                    ),
-                    SizedBox(
-                      width: 40,
-                    ),
-                    Container(
-                      width: 300,
-                      height: 280,
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [
-                              Colors.teal,
-                              // Colors.tealAccent,
-                              // Colors.black87,
-                              Colors.redAccent,
-                              Colors.yellow
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                'Europe travel',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                              SvgPicture.asset('assets/icons/Frame 43.svg'),
                             ],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.centerRight),
+                          ),
+                          const Spacer(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '1882 8245 9831 0505',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                              const Spacer(),
+                              SvgPicture.asset(
+                                'assets/icons/touch--2 1.svg',
+                                // fit: BoxFit.scaleDown,
+                                width: 28,
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          const Text(
+                            '05 / 25',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                          const Spacer(
+                            flex: 6,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text.rich(
+                                TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: '£ 3,710.',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    TextSpan(text: '80'),
+                                  ],
+                                  style: TextStyle(
+                                      fontSize: 30, color: Colors.white),
+                                ),
+                              ),
+                              SvgPicture.asset(
+                                'assets/icons/Solid.svg',
+                                // fit: BoxFit.scaleDown,
+                                width: 95,
+                              ),
+                            ],
+                          )
+                        ],
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  Container(
+                    width: 300,
+                    height: 280,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [
+                            Colors.teal,
+                            // Colors.tealAccent,
+                            // Colors.black87,
+                            Colors.redAccent,
+                            Colors.yellow
+                          ],
+                          begin: Alignment.bottomLeft,
+                          end: Alignment.centerRight),
+                    ),
+                  )
+                ],
               ),
             ),
           ),
